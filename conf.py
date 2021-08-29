e7ed9877b7f1ef8ff8714860a4fe25e9c62f0db1
LiuHui <liuhui@hortorgames.com>
2021-08-29 15:24:42 +0800

    [removed] project

--- conf.py
+++
@@ -1,5 +0,0 @@
-class Config(object):
-    DATABASE_PATH = "./updb.sqlite"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + DATABASE_PATH
-    SECRET_KEY = "dkjafkldjakfjdkafjdlkajfkldajfkldajklfjalkjdka"
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
--- web.py
+++
@@ -1,7 +0,0 @@
-from app import create_app
-from conf import config
-
-app = create_app(config)
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
